31ae8207c7bdeb7235eb6c77352b5d576a0fb5f3
TheWiseSailor <rudasilljay@gmail.com>
2024-01-06 17:41:22 -0500

    basic layout but its  rendering propertyoftheday instead of properties.js so i will fix that

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,12 +1,51 @@
 // src/components/Properties/Properties.js
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Properties.css";
+import viewProperties from "./ViewProperties"; 
+
 const Properties = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="mainpropertycontainer">
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search Wilmington Properties"
+        />
+        <button className="search-button">Search</button>
+      </div>
 
-      <h2>this will be a search bar</h2>
-
+      <div className="property-container">
+        {viewProperties.map((viewProperty) => (
+          <div
+            key={viewProperty.id}
+            className="property-card"
+            data-aos="fade-down"
+            data-aos-duration="3000"
+          >
+            <img
+              src={viewProperty.image}
+              alt={`Property ${viewProperty.id} - ${viewProperty.address}`}
+            />
+            <div className="property-details">
+              <p>{viewProperty.price}</p>
+              <p>{viewProperty.details}</p>
+              <p className="property-address">{viewProperty.address}</p>
+              <Link key={viewProperty.id} to={`/property/${viewProperty.id}`}>
+                <button className="green-button">View!</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/Properties/ViewProperties.js
@@ -0,0 +1,27 @@
+// src/components/Properties/ViewProperties.js
+const viewProperties = [
+  {
+    id: 1,
+    image: "property1.jpg",
+    price: "$600,000",
+    details: "4 Bed, 3 Bath",
+    address: "456 Oak St",
+  },
+  {
+    id: 2,
+    image: "property2.jpg",
+    price: "$750,000",
+    details: "5 Bed, 4 Bath",
+    address: "789 Maple Ave",
+  },
+  {
+    id: 3,
+    image: "property3.jpg",
+    price: "$450,000",
+    details: "3 Bed, 2 Bath",
+    address: "101 Pine Ln",
+  },
+  // Add more properties as needed
+];
+
+export default viewProperties;
