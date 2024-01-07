a49a7866d14d6487cce4437229e41494f6b7b241
TheWiseSailor <rudasilljay@gmail.com>
2024-01-07 16:39:00 -0500

    got tyhe separate conditional renderings debugged ( as of now It works)

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,45 +1,69 @@
-// src/components/Properties/Properties.js
-import React, { useState } from "react";
+// src\components\Properties\Properties.js
+
+import React from "react";
 import { Link } from "react-router-dom";
-import "./Properties.css";
-import viewProperties from "./ViewProperties"; 
+
+import "./Properties.css"; 
 
 const Properties = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
+  const otherProperties = [
+    {
+      id: 5,
+      image: "URL_TO_IMAGE_5",
+      price: "$950,000 / $9-15k Month",
+      details: "4 beds / 3 baths / 2800 sqft",
+      address: "Asheville, NC",
+    },
+    {
+      id: 6,
+      image: "URL_TO_IMAGE_6",
+      price: "$550,000 / $5-10k Month",
+      details: "2 beds / 2 baths / 1500 sqft",
+      address: "Greenville, SC",
+    },
+    {
+      id: 7,
+      image: "URL_TO_IMAGE_7",
+      price: "$1,500,000 / $15-25k Month",
+      details: "6 beds / 5 baths / 4000 sqft",
+      address: "Hilton Head, SC",
+    },
+    {
+      id: 8,
+      image: "URL_TO_IMAGE_8",
+      price: "$1,100,000 / $11-18k Month",
+      details: "5 beds / 4.5 baths / 3500 sqft",
+      address: "Savannah, GA",
+    },
+  ];
 
   return (
-    <div className="mainpropertycontainer">
-      <div className="search-container">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          placeholder="Search Wilmington Properties"
-        />
-        <button className="search-button">Search</button>
-      </div>
+    <div>
 
       <div className="property-container">
-        {viewProperties.map((viewProperty) => (
+        <h1
+          className="Todays-Title"
+          data-aos="fade-down"
+          data-aos-duration="3000"
+        >
+          Other Property Listings
+        </h1>
+        {otherProperties.map((property) => (
           <div
-            key={viewProperty.id}
+            key={property.id}
             className="property-card"
             data-aos="fade-down"
             data-aos-duration="3000"
           >
             <img
-              src={viewProperty.image}
-              alt={`Property ${viewProperty.id} - ${viewProperty.address}`}
+              src={property.image}
+              alt={`Property ${property.id} - ${property.address}`}
             />
             <div className="property-details">
-              <p>{viewProperty.price}</p>
-              <p>{viewProperty.details}</p>
-              <p className="property-address">{viewProperty.address}</p>
-              <Link key={viewProperty.id} to={`/property/${viewProperty.id}`}>
+              <p>{property.price}</p>
+              <p>{property.details}</p>
+              <p className="property-address">{property.address}</p>
+              <Link key={property.id} to={`/properties/${property.id}`}>
                 <button className="green-button">View!</button>
               </Link>
             </div>
--- src/components/Properties/ViewProperties.js
+++ src/components/Properties/ViewProperties.js
@@ -1,27 +1,49 @@
-// src/components/Properties/ViewProperties.js
-const viewProperties = [
-  {
-    id: 1,
-    image: "property1.jpg",
-    price: "$600,000",
-    details: "4 Bed, 3 Bath",
-    address: "456 Oak St",
-  },
-  {
-    id: 2,
-    image: "property2.jpg",
-    price: "$750,000",
-    details: "5 Bed, 4 Bath",
-    address: "789 Maple Ave",
-  },
-  {
-    id: 3,
-    image: "property3.jpg",
-    price: "$450,000",
-    details: "3 Bed, 2 Bath",
-    address: "101 Pine Ln",
-  },
-  // Add more properties as needed
-];
+// src\components\Properties\ViewProperties.js
 
-export default viewProperties;
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./Properties.css"; 
+
+const ViewProperties = ({ properties }) => {
+  const { id } = useParams();
+
+  const property = properties.find((property) => property.id === parseInt(id));
+
+  if (!property) {
+    return <div>Property not found</div>;
+  }
+
+  return (
+    <div className="PropertyOfTheDayBg">
+      <Carousel className="Carousel" showThumbs={false}>
+        {property.images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              className="PropertyImage"
+              alt={`Property ${property.id} - ${index + 1}`}
+            />
+          </div>
+        ))}
+      </Carousel>
+
+      <div className="DetailsAndForm">
+        <div className="PropertyDetails">
+          <div className="DetailPropertyNameSize text-1xl">
+            <div className="PropertyDetailsTitle font-bold">
+              About This Property!
+            </div>
+            <p>{property.price}</p>
+            <p>{property.details}</p>
+            <p>{property.address}</p>
+            <p>Agent: {property.agent}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewProperties;
