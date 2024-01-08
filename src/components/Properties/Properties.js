d9378b44b003cd92c61fd29d408fd975264f4f60
TheWiseSailor <rudasilljay@gmail.com>
2024-01-07 20:14:35 -0500

    simple base p;ate of all the realestate listings are listed

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,9 +1,8 @@
-// src\components\Properties\Properties.js
+// src/components/Properties/Properties.js
 
 import React from "react";
 import { Link } from "react-router-dom";
-
-import "./Properties.css"; 
+import "./Properties.css";
 
 const Properties = () => {
   const otherProperties = [
@@ -35,44 +34,84 @@ const Properties = () => {
       details: "5 beds / 4.5 baths / 3500 sqft",
       address: "Savannah, GA",
     },
+  
+  {
+    id: 9,
+    image: "URL_TO_IMAGE_9",
+    price: "$750,000 / $7-12k Month",
+    details: "3 beds / 2.5 baths / 2300 sqft",
+    address: "Charleston, SC",
+  },
+  {
+    id: 10,
+    image: "URL_TO_IMAGE_10",
+    price: "$620,000 / $6-11k Month",
+    details: "4 beds / 3 baths / 2600 sqft",
+    address: "Myrtle Beach, SC",
+  },
+  {
+    id: 11,
+    image: "URL_TO_IMAGE_11",
+    price: "$890,000 / $8-14k Month",
+    details: "5 beds / 4 baths / 3200 sqft",
+    address: "Columbia, SC",
+  },
+  {
+    id: 12,
+    image: "URL_TO_IMAGE_12",
+    price: "$1,200,000 / $12-20k Month",
+    details: "6 beds / 5.5 baths / 4000 sqft",
+    address: "Atlanta, GA",
+  },
+  {
+    id: 13,
+    image: "URL_TO_IMAGE_13",
+    price: "$580,000 / $5-9k Month",
+    details: "3 beds / 2 baths / 1800 sqft",
+    address: "Augusta, GA",
+  },
+  {
+    id: 14,
+    image: "URL_TO_IMAGE_14",
+    price: "$950,000 / $9-15k Month",
+    details: "4 beds / 3.5 baths / 3400 sqft",
+    address: "Macon, GA",
+  },
+
   ];
-
   return (
-    <div>
+    <div className="PropertyListingOfTheDayBg">
+      <h1
+        className="PropertyListingDetailsTitle"
+        data-aos="fade-down"
+        data-aos-duration="3000"
+      >
 
-      <div className="property-container">
-        <h1
-          className="Todays-Title"
+      </h1>
+      {otherProperties.map((property) => (
+        <div
+          key={property.id}
+          className="property-card-container"
           data-aos="fade-down"
           data-aos-duration="3000"
         >
-          Other Property Listings
-        </h1>
-        {otherProperties.map((property) => (
-          <div
-            key={property.id}
-            className="property-card"
-            data-aos="fade-down"
-            data-aos-duration="3000"
-          >
-            <img
-              src={property.image}
-              alt={`Property ${property.id} - ${property.address}`}
-            />
-            <div className="property-details">
-              <p>{property.price}</p>
-              <p>{property.details}</p>
-              <p className="property-address">{property.address}</p>
-              <Link key={property.id} to={`/properties/${property.id}`}>
-                <button className="green-button">View!</button>
-              </Link>
-            </div>
+          <img
+            src={property.image}
+            alt={`Property ${property.id} - ${property.address}`}
+            className="PropertyListingImage"
+          />
+          <div className="PropertyListingDetails">
+            <p>{property.price}</p>
+            <p>{property.details}</p>
+            <p className="property-address">{property.address}</p>
+            <Link key={property.id} to={`/properties/${property.id}`}>
+              <button className="green-button">View!</button>
+            </Link>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Properties;
-//add back that search bar
