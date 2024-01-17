d7d61ffa5aa7a62379bf95feb5ffe1a2576dbd85
TheWiseSailor <rudasilljay@gmail.com>
2024-01-17 18:08:55 -0500

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import "../App.css";
 
 const Footer = () => {
   return (
-    <footer className="footer fixed bottom-0 w-full bg-gray-200 pt-3 pb-3">
+    <footer className="footer fixed bottom-0 w-full bg-gray-200  ">
       <div>
-        <p>Copyright &copy; 2023 All rights reserved | Moore Real Estate</p>
+        <p className="FooterText">Copyright &copy; 2023 All rights reserved | Moore Real Estate</p>
       </div>
     </footer>
   );
