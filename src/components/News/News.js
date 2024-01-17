4ad2f751928d5f43d2c7ac843bb0df0fb2d5e29b
TheWiseSailor <rudasilljay@gmail.com>
2024-01-17 17:59:43 -0500

    news page update pt1

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,42 +1,44 @@
 import React from 'react';
+import Img1 from '../News/NewsImages/Img1.jpg'; // Make sure to provide the correct path
+
 import "../News/News.css";
 
 const NewsPage = () => {
     return (
         <div className='MainNewsContainer'>
-            <h1>Latest Real Estate News in Wilmington, NC</h1>
+            <h1 className='news1'>Latest Real Estate News in Wilmington, NC</h1>
 
             <section className="article">
-                <img src="" alt="Downtown Development" className="article-image" />
+                <img src={Img1} alt="Downtown Development" className="article-image" />
                 <div className="article-content">
-                    <h2>New Developments in Downtown Wilmington</h2>
+                    <h2 className='news2'>New Developments in Downtown Wilmington</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt justo eu mauris faucibus, a congue libero auctor.</p>
                     <p><a href="#">Read more</a></p>
                 </div>
             </section>
 
             <section className="article">
-                <img src="" alt="Rising Home Prices" className="article-image" />
+                <img src={Img1} alt="Rising Home Prices" className="article-image" />
                 <div className="article-content">
-                    <h2>Home Prices on the Rise</h2>
+                    <h2 className='news2'>Home Prices on the Rise</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt justo eu mauris faucibus, a congue libero auctor.</p>
                     <p><a href="#">Read more</a></p>
                 </div>
             </section>
 
             <section className="article">
-                <img src="" alt="New Suburban Communities" className="article-image" />
+                <img src={Img1} alt="New Suburban Communities" className="article-image" />
                 <div className="article-content">
-                    <h2>Emerging Suburban Communities</h2>
+                    <h2 className='news2'>Emerging Suburban Communities</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt justo eu mauris faucibus, a congue libero auctor.</p>
                     <p><a href="#">Read more</a></p>
                 </div>
             </section>
 
             <section className="article">
-                <img src="" alt="Luxury Real Estate Market" className="article-image" />
+                <img src={Img1} alt="Luxury Real Estate Market" className="article-image" />
                 <div className="article-content">
-                    <h2>Luxury Real Estate Market Booming</h2>
+                    <h2 className='news2'>Luxury Real Estate Market Booming</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt justo eu mauris faucibus, a congue libero auctor.</p>
                     <p><a href="#">Read more</a></p>
                 </div>
