513d47791ffb86d8302466e81cbe1c41a16a3718
TheWiseSailor <rudasilljay@gmail.com>
2024-01-17 22:24:21 -0500

    more aos

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -27,28 +27,30 @@ const NewsPage = () => {
                
                 </div>
             </section>
-
+            <AOSInitializer />
             <section className="article"data-aos="fade-down"
         data-aos-duration="3000">
                 <img src={Img2} alt="Rising Home Prices" className="article-image" />
-                <div className="article-content">
+                <div className="article-content"data-aos="fade-down"
+        data-aos-duration="3000">
                     <h2 className='news2'>Home Prices on the Rise</h2>
                     <p className='p1'>Home prices in Wilmington, North Carolina, are on the rise, reflecting the increasing demand for housing in the region.</p>
                  
                 </div>
             </section>
-
+            <AOSInitializer />
             <section className="article"data-aos="fade-down"
         data-aos-duration="3000">
                 <img src={Img3} alt="New Suburban Communities" className="article-image" />
-                <div className="article-content">
+                <div className="article-content"data-aos="fade-down"
+        data-aos-duration="3000">
                     <h2 className='news2'>Emerging Suburban Communities</h2>
                     <p className='p1'>Emerging suburban communities in Wilmington, North Carolina, are experiencing notable growth and transformation in 2023. 
 </p>
                 
                 </div>
             </section>
-
+            <AOSInitializer />
             <section className="article"data-aos="fade-down"
         data-aos-duration="3000">
                 <img src={Img4} alt="Luxury Real Estate Market" className="article-image" />
