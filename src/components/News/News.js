bf6bfafe193fd566a733d13463368ebf973069be
TheWiseSailor <rudasilljay@gmail.com>
2024-01-17 22:22:08 -0500

    aos features

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -3,24 +3,33 @@ import Img1 from '../News/NewsImages/Img1.jpg';
 import Img2 from '../News/NewsImages/Img2.jpg';
 import Img3 from '../News/NewsImages/Img3.jpg';
 import Img4 from '../News/NewsImages/Img4.jpg';
+import AOSInitializer from "../AOS/AOSInitializer";
 
 import "../News/News.css";
 
 const NewsPage = () => {
     return (
+        
         <div className='MainNewsContainer'>
-            <h1 className='news1'>Latest Real Estate News in Wilmington, NC</h1>
-
-            <section className="article">
+              
+   <AOSInitializer />
+            <h1 className='news1' data-aos="fade-down"
+        data-aos-duration="3000">Latest Real Estate News in Wilmington, NC</h1>
+   
+   <AOSInitializer />
+            <section className="article" data-aos="fade-down"
+        data-aos-duration="3000">
                 <img src={Img1} alt="Downtown Development" className="article-image" />
-                <div className="article-content">
+                <div className="article-content"data-aos="fade-down"
+        data-aos-duration="3000">
                     <h2 className='news2'>New Developments in Downtown Wilmington</h2>
                     <p className='p1'>Downtown Wilmington sees a resurgence with new developments, bringing modern architecture and cultural spaces to contribute to the city's urban vibrancy.</p>
                
                 </div>
             </section>
 
-            <section className="article">
+            <section className="article"data-aos="fade-down"
+        data-aos-duration="3000">
                 <img src={Img2} alt="Rising Home Prices" className="article-image" />
                 <div className="article-content">
                     <h2 className='news2'>Home Prices on the Rise</h2>
@@ -29,7 +38,8 @@ const NewsPage = () => {
                 </div>
             </section>
 
-            <section className="article">
+            <section className="article"data-aos="fade-down"
+        data-aos-duration="3000">
                 <img src={Img3} alt="New Suburban Communities" className="article-image" />
                 <div className="article-content">
                     <h2 className='news2'>Emerging Suburban Communities</h2>
@@ -39,9 +49,11 @@ const NewsPage = () => {
                 </div>
             </section>
 
-            <section className="article">
+            <section className="article"data-aos="fade-down"
+        data-aos-duration="3000">
                 <img src={Img4} alt="Luxury Real Estate Market" className="article-image" />
-                <div className="article-content">
+                <div className="article-content"data-aos="fade-down"
+        data-aos-duration="3000">
                     <h2 className='news2'>Luxury Real Estate Market Booming</h2>
                     <p className='p1'>Wilmington's luxury real estate market is currently booming, witnessing a surge in demand and heightened interest in high-end properties.</p>
              
